5db89e13d65caec05c8609cd36f33071118eebdc
Timon Heinis <tibohei@bluewin.ch>
2016-05-20 23:01:38 +0200

    bring function to tabs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+(function() {
+
 var app = angular.module('competition',[]);
+
+app.controller('tabController',function(){
+  this.tab = 1;
+
+  this.setTab = function(vTab){
+    this.tab = vTab;
+  };
+
+  this.isSet = function(checkTab){
+    return this.tab === checkTab;
+  };
+});
+
+})();
