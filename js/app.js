80a093d4ecf1ffee23038679da60db006099ca6c
Timon Heinis <tibohei@bluewin.ch>
2016-05-23 15:07:56 +0200

    put data in json, implement functions to load data into form

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@
 
 var app = angular.module('competition',[]);
 
-app.controller('competitionController',function(){
-  this.teams = teams;
-});
+app.controller('competitionController', ['$http', function($http){
+  var competition = this;
+  competition.teams = [];
+  $http.get('data.json').success(function(data){
+    competition.teams = data;
+  });
+}]);
 
 app.controller('tabController',function(){
   this.tab = 1;
@@ -21,47 +25,46 @@ app.controller('tabController',function(){
 app.controller("dataController", function(){
 
     this.team = {};
+    this.teamNames = ['01a', '02a', '03a'];
 
-    this.addData = function(team){
+    this.addData = function(teams){
       this.team.modifiedOn = Date.now();
       teams.push(this.team);
       this.team = {};
     };
+
+    this.loadData = function(team){
+      this.team = team;
+    };
+
+    this.clearForm = function(){
+      this.team = {};
+    };
+
   });
 
 
 /* Something with this objects is wrong... */
+/*
 var teams = [
   {
     name: '01a',
     mass: 2.0,
     sizeOK: false,
     modifiedOn: 0
-    /*system: {
-      mass: 2.0,
-      sizeOK: false
-    }*/
   },
   {
     name: '02a',
     mass: 3.0,
     sizeOK: false,
     modifiedOn: 0
-    /*system: {
-      mass: 2.0,
-      sizeOK: false
-    }*/
   },
   {
     name: '03a',
     mass: 2.5,
     sizeOK: false,
     modifiedOn: 0
-    /*system: {
-      mass: 2.0,
-      sizeOK: false
-    }*/
   }
-];
+];  */
 
 })();
